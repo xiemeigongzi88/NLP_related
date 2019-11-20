68f062c0b73c8b63a24ebfa71c1e99522f9e0873
xiemeigongzi88 <43561060+xiemeigongzi88@users.noreply.github.com>
2019-11-20 16:42:13 -0600

    Add files via upload

---
+++ Sentiment Analysis/Yelp Reviews in Dallas/test_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 15:20:04 2019
+
+@author: sxw17
+"""
+
+
+import os 
+os.getcwd()
+
+path ="C:\\Users\\sxw17\\Desktop\\NLP\\Project"
+os.chdir(path)
+
+
